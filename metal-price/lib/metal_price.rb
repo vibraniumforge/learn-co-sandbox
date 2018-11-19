9ab761315f9f80da44714a9e8d70ed5ea6e609db
Greg Morgan <gregm9999@gmail.com>
2018-11-19 02:16:34 +0000

    more

--- metal-price/lib/metal_price.rb
+++ metal-price/lib/metal_price.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 require "nokogiri"
 require "pry"
 require_relative "./metal/price/version.rb"
-require_relative "./cli.rb"
+# require_relative "./cli.rb"
 module MetalPrice
   
 end
